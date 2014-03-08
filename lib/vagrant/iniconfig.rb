046c63bce5d55a939bfe9541bffcb574d5af1133
Daniel Enman <enmand@gmail.com>
2014-03-08 12:19:05 -0400

    Initial commit of vagrant-inifile

---
+++ lib/vagrant/iniconfig.rb
@@ -0,0 +1,7 @@
+require "vagrant/iniconfig/version"
+
+module Vagrant
+  module Iniconfig
+    # Your code goes here...
+  end
+end
---
+++ lib/vagrant/iniconfig/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module Iniconfig
+    VERSION = "0.0.1"
+  end
+end
