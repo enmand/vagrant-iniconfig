efdf4a28fdb087e93d8ca5063821788667193f99
Daniel Enman <enmand@gmail.com>
2014-03-09 15:32:37 -0300

    Add self.source_path to vagrant-iniconfig

--- lib/vagrant-iniconfig.rb
+++ lib/vagrant-iniconfig.rb
@@ -1,2 +1,10 @@
-require "vagrant-inifile/plugin"
-require "vagrant-inifile/version"
+require_relative "vagrant-iniconfig/plugin"
+require_relative "vagrant-iniconfig/version"
+
+module VagrantPlugins
+	module IniConfig
+		def self.source_root
+			@source_root ||= Pathname.new(File.expand_path('../../', __FILE__))
+		end
+	end
+end
